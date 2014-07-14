c710695bba8560035ce06f2917f13dcaad7609bc
Sachi Sanghavi <sanghavi.sachi@gmail.com>
2014-07-14 09:17:01 -0700

    Create main.py
    wip

---
+++ main.py
@@ -0,0 +1,157 @@
+from xlrd import open_workbook
+from pyeda.inter import *
+
+wb = open_workbook(filename='1.xls', formatting_info=True)
+matrixSheet = wb.sheet_by_name('Interlock Matrix')
+stringsSheet = wb.sheet_by_name('Interlock Strings')
+interlockNumArray = []
+whereUsedNumArray = []
+ioMemNumArray = []
+
+
+def is_flat(name):
+	if not isinstance(name, str):
+		for item in name:
+			if item.find('ILK_') or item.find('NOT_ILK_'):
+				return False
+	else:
+		if name.find('ILK_') or name.find('NOT_ILK_'):
+			return False
+	return True
+
+def flatten_helper(name, addStr):
+	temp = []
+	index = 0
+	temp.append(addStr)
+	# find interlock on worksheet
+	for num in interlockNumArray:
+		if name == stringsSheet.cell(num, 11).value:
+			index = interlockNumArray.index(num)
+	start = ioMemNumArray[index] + 1
+	stop = whereUsedNumArray[index] - 1
+	c1 = stringsSheet.col_values(11, start, stop)
+	c2 = stringsSheet.col_values(26, start, stop)
+	for item in c1:
+		temp.append(item)
+		print(item)
+	for item in c1:
+		temp.append(item)
+	return temp
+
+def flatten_expr(array, name):
+	newName, intermediateName = '', ''
+	addStr = ''
+	if name.find('ILK_') != -1:
+		newName = flatten_helper(name[8:], addStr)
+	elif name.find('NOT_ILK_') != -1:
+		# remove NOT_ from name
+		temp = name.partition('NOT_')
+		itermediateName = temp[0] + temp[2]
+		addStr = '~'
+		newName = flatten_helper(intermediateName[8:], addStr)
+	elif name.find('NOT_DO') != -1:
+		newName = '~' + name[4:]
+	return newName
+
+def match_helper(col, soName):
+	for item in col:
+		if item.find(soName) != -1:
+			return True
+	return False
+
+##
+# found soName in given search columms?
+def match(col1, col2, soName):
+	found1 = match_helper(col1, soName)
+	found2 = match_helper(col2, soName)
+	if found1 or found2:
+		return True
+	return False
+
+def main():
+	#wb = open_workbook(filename='1.xls', formatting_info=True)
+	#matrixSheet = wb.sheet_by_name('Interlock Matrix')
+	soArray = matrixSheet.col_values(1, 3, 179)
+	doiArray = matrixSheet.row_values(2, 14, 146)
+
+	# number of rows and columns in matrix
+	nrows = len(soArray) # 176
+	ncols = len(doiArray) # 132
+
+	colourMap = dict()
+
+	# dark blue rgb(0, 51, 102)
+	# light blue rbd(204, 255, 255)
+	for i, so in enumerate(soArray):
+		dependencyList = []
+		# matirx begins at row 3, col 14
+		colourData = matrixSheet.row(i+3)
+		for j, doi in enumerate(doiArray):
+			xfx = matrixSheet.cell_xf_index(i+3, j+14)
+			xf = wb.xf_list[xfx]
+			bgx = xf.background.pattern_colour_index
+			rgb = wb.colour_map[bgx]
+			if rgb == (0, 51, 102):
+				dependencyList.append(doi)
+
+		colourMap[so] = dependencyList
+
+	# list of rownums for interlock number and where used
+	#stringsSheet = wb.sheet_by_name('Interlock Strings')
+	filterArray = stringsSheet.col_values(0)
+	#interlockNumArray = []
+	#whereUsedNumArray = []
+	#ioMemNumArray = []
+	for index, item in enumerate(filterArray):
+		if item == "Interlock Number:":
+			interlockNumArray.append(index)
+		elif item == "Where Used:":
+			whereUsedNumArray.append(index)
+		elif item == "I/O Members:":
+			ioMemNumArray.append(index)
+
+
+	dependencyMap = dict()
+
+	for soName in soArray:
+		dependencyMap[soName] = []
+		for index, item in enumerate(whereUsedNumArray):
+			check = False
+			if index == (len(whereUsedNumArray) - 1):
+				searchCol1 = stringsSheet.col_values(11, item+1)
+				searchCol2 = stringsSheet.col_values(26, item+1)
+			else:
+				searchCol1 = stringsSheet.col_values(11, item+1, interlockNumArray[index+1])
+				searchCol2 = stringsSheet.col_values(26, item+1, interlockNumArray[index+1])
+
+			found = match(searchCol1, searchCol2, soName)
+			if not found:
+				continue
+			break
+		dependencyMap[soName].append(stringsSheet.cell(interlockNumArray[index], 11).value)
+		start = ioMemNumArray[index]+1
+		stop = whereUsedNumArray[index]-1
+		c1 = stringsSheet.col_values(11, start, stop)
+		c2 = stringsSheet.col_values(26, start, stop)
+		for doName in c1:
+			if doName == '':
+				continue
+			dependencyMap[soName].append(doName)
+		for doName in c2:
+			if doName == '':
+				continue
+			dependencyMap[soName].append(doName)
+
+	
+	# print(dependencyMap['SO_102'])
+
+	for k, v in dependencyMap.items():
+		for index, item in enumerate(v):
+			newItem = flatten_expr(v, item)
+			v[index] = newItem
+
+	print(dependencyMap['SO_000'])
+
+
+if __name__ == '__main__':
+	main()
