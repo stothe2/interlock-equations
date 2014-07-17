367a86ef341ecd8fd968fc67a31bd5bcccfe378d
Sachi Sanghavi <sanghavi.sachi@gmail.com>
2014-07-17 05:10:07 -0700

    Create Tree.py
    wip

---
+++ Tree.py
@@ -0,0 +1,108 @@
+class Tree:
+
+	class _Node:
+		def __init__(self, data):
+			self.data = data
+			# black, True: visited
+			# red, False: not visited
+			self.colour = False
+			self.parent = None
+			self.children = list()
+
+	def __init__(self):
+		self._root = None
+		self._elements = 0
+
+	def has_parent(self, p):
+		n = self._convert(p)
+		return n.parent != None
+
+	def has_children(self, p):
+		n = self._convert(p)
+		return len(n.children) != 0
+
+	def insert_root(self, data):
+		if self._root:
+			raise Exception('TreeNotEmptyException')
+		self._root = self._Node(data)
+		self._elements = self._elements + 1
+		return self._root
+
+	def insert_child(self, p, data):
+		p = self._convert(p)
+		n = self._Node(data)
+		n.parent = p
+		p.children.append(n)
+		self._elements = self._elements + 1
+		return n
+
+	def root(self):
+		if not self._root:
+			raise Exception('EmptyTreeException')
+		return self._root
+
+	def get(self):
+		return self.data
+
+	def _convert(self, p):
+		# check for isinstance
+		if not p:
+			raise Exception('not really a position')
+		# hmm, not really typecasting
+		n = p
+		return n
+
+	def size(self):
+		return self._elements
+
+	def empty(self):
+		return len(self._elements) == 0
+
+	def pre(self):
+		self._pre_traversal(self._root)
+
+	def _pre_traversal(self, n):
+		if n:
+			print(n.data)
+			if self.has_children(n):
+				for child in n.children:
+					n = child
+					self._pre_traversal(n)
+
+	def has_ilk(self):
+		array = self._to_list(self._root, [])
+		for item in array:
+			if item.data.find('ILK_') != -1 and not item.colour:
+				return True
+		return False
+
+	def get_ilk(self):
+		array = self._to_list(self._root, [])
+		for n in array:
+			if n.data.find('ILK_') != -1 and not n.colour:
+				n.colour = True
+				return n, n.data[8:]
+		raise Exception('NoILKPresentException')
+
+	def _to_list(self, n, array):
+		if n:
+			array.append(n)
+			if self.has_children(n):
+				for child in n.children:
+					n = child
+					self._to_list(n, array)
+		return array
+
+
+
+'''
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		root = self._root
+		if not root:
+			StopIteration
+		else:
+			# ...
+'''
